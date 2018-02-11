6261d7adb67d93e4c02415d0f7c5eb7c0d423c69
Amar Chaudhari <amar.chaudhari@colorado.edu>
2018-02-11 11:59:44 -0700

    Updated alexa app file

--- vivonet_site/intents/alexa.py
+++ vivonet_site/intents/alexa.py
@@ -7,15 +7,7 @@ logger = logging.getLogger("django_alexa.views")
 
 @intent(app='intents')
 def LaunchRequest(session):
-    """
-    Hogwarts is a go
-    ---
-    launch
-    start
-    run
-    begin
-    open
-    """
+
     return ResponseBuilder.create_response(message="Welcome to ViVonet skill. Using our skill you can simplify network management.",
                                            end_session=True)
 
@@ -23,5 +15,4 @@ def LaunchRequest(session):
 def CreateNetwork(session,from_city,to_city):
     speech_text = '<speak>Vivonet will setup a 100 milli second path from {0} to {1}</speak>'.format(from_city,to_city)
     reprompt_text = '<speak>What path do you want to setupt? You can say from Denver to SF</speak>'
-    print from_city, to_city
     return ResponseBuilder.create_response(message=speech_text,message_is_ssml=True)
