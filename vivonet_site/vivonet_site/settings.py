53a358c548b3cfd136b0927150532fa1bc03c96c
Amar Chaudhari <amar.chaudhari@colorado.edu>
2018-03-14 00:38:33 -0600

    enabled CORS

--- vivonet_site/vivonet_site/settings.py
+++ vivonet_site/vivonet_site/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     'django_alexa',
     'intents',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'vivonet_site.urls'
 
