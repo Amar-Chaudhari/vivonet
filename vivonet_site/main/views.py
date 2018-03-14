f7e8df6b245059931f3a06e9ce672d4489722cdc
atharvak21 <atharvak21@gmail.com>
2018-03-13 20:08:15 -0600

    Atharva (#5)
    * IEv1

--- vivonet_site/main/views.py
+++ vivonet_site/main/views.py
@@ -18,10 +18,9 @@ def network_topology(request):
     return render(request, 'Network_Topology.html')
 
 def testdb(request):
-    c = ComputeAndPush('198.11.21.36', 'DEN', 'SFO', 'least latency')
-    db = c.create_flows()
+    c = ComputeAndPush('198.11.21.36', 'DEN', 'SFO', 'least_hop_count')
+    db = c.intentEngine()
     return HttpResponse(db)
-
 
 @api_view(['GET'])
 def dropdown_data(request):
