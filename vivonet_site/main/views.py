d0672b2f9ee36d109712fd851805e3d4a4f9fd8e
Atharva Kaluskar <atharvak@cu-vpn-colorado-edu-198.11.28.182.int.colorado.edu>
2018-03-13 21:57:31 -0600

    Resolving views

--- vivonet_site/main/views.py
+++ vivonet_site/main/views.py
@@ -21,52 +21,7 @@ def network_topology(request):
 
 
 def testdb(request):
-<<<<<<< HEAD
-<<<<<<< HEAD
     c = ComputeAndPush('10.0.1.200', 'DEN', 'SFO', 'least_latency')
     db = c.intentEngine()
     return HttpResponse(db)
-=======
-=======
-    c = ComputeAndPush('198.11.21.36', 'DEN', 'SFO', 'least_hop_count')
-    db = c.intentEngine()
-    return HttpResponse(db)
 
->>>>>>> 9de247b73bf824aaebd0a19764edc7156d521d1e
-@api_view(['GET'])
-def dropdown_data(request):
-    """
-    {'1':
-        {
-            'name' : 'Denver to Los Angeles - least latency',
-            'path' : 'dpid - dpid -dpid',
-        },
-    '2':
-        {
-            'name' : 'Denver to Los Angeles - least latency',
-            'path' : 'dpid - dpid -dpid',
-        },
-    }
-    """
-    data = {}
-    try:
-        if request.method == 'GET':
-            intents_all = Intent_Data.objects.all()
-            i=1
-            for intent in intents_all:
-                source = Customer.objects.filter(Prefix=intent.Source_IP).values_list('location',flat=True)[0]
-                destination = Customer.objects.filter(Prefix=intent.Destination_IP).values_list('location',flat=True)[0]
-                intent_type = intent.Intent_Type
-                temp = {}
-                temp['name'] = "{0} to {1} - {2}".format(source,destination,intent_type)
-                temp['path'] = intent.Path
-                data[i] = temp
-                i += 1
-            return HttpResponse(data)
-    except:
-<<<<<<< HEAD
-        return Response(data)
->>>>>>> f7e8df6b245059931f3a06e9ce672d4489722cdc
-=======
-        return HttpResponse(data)
->>>>>>> 9de247b73bf824aaebd0a19764edc7156d521d1e
