d301925515a690494fafd81574d886eee382fbc6
Amar Chaudhari <amar.chaudhari@colorado.edu>
2018-03-14 00:39:45 -0600

    enabled CORS

--- vivonet_site/vivonet_site/settings.py
+++ vivonet_site/vivonet_site/settings.py
@@ -86,6 +86,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
