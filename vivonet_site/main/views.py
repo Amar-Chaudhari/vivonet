2eb717eef2bcafbe87059227a296d9c52ed61874
Amar Chaudhari <amar.chaudhari@colorado.edu>
2018-03-13 21:25:36 -0600

    Changed API

--- vivonet_site/main/views.py
+++ vivonet_site/main/views.py
@@ -2,14 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
-from rest_framework import status
 from rest_framework.decorators import api_view
+import json
 from rest_framework.response import Response
-
 from intents.intent_engine import *
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+
 
 # Create your views here.
 def index_view(request):
@@ -24,6 +21,7 @@ def testdb(request):
     c = ComputeAndPush('198.11.21.36', 'DEN', 'SFO', 'least_hop_count')
     db = c.intentEngine()
     return HttpResponse(db)
+
 
 @api_view(['GET'])
 def dropdown_data(request):
@@ -44,16 +42,17 @@ def dropdown_data(request):
     try:
         if request.method == 'GET':
             intents_all = Intent_Data.objects.all()
-            i=1
+            i = 1
             for intent in intents_all:
-                source = Customer.objects.filter(Prefix=intent.Source_IP).values_list('location',flat=True)[0]
-                destination = Customer.objects.filter(Prefix=intent.Destination_IP).values_list('location',flat=True)[0]
+                source = Customer.objects.filter(Prefix=intent.Source_IP).values_list('location', flat=True)[0]
+                destination = Customer.objects.filter(Prefix=intent.Destination_IP).values_list('location', flat=True)[
+                    0]
                 intent_type = intent.Intent_Type
                 temp = {}
-                temp['name'] = "{0} to {1} - {2}".format(source,destination,intent_type)
+                temp['name'] = "{0} to {1} - {2}".format(source, destination, intent_type)
                 temp['path'] = intent.Path
                 data[i] = temp
                 i += 1
-            return HttpResponse(data)
+            return Response(json.dumps(data))
     except:
-        return HttpResponse(data)
+        return Response(json.dumps(data))
