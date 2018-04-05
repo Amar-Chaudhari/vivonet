145237deef0e76b3ecd0a9739058e5ec886967e5
Amar Chaudhari <amar.chaudhari@colorado.edu>
2018-04-04 19:00:16 -0600

    Changed models and updated alexa code for hopcount

--- vivonet_site/intents/alexa.py
+++ vivonet_site/intents/alexa.py
@@ -1,9 +1,8 @@
 import logging
 
+import duo_client
 from django_alexa.api import fields, intent, ResponseBuilder
 
-from main.models import *
-import duo_client
 from intents.intent_engine import *
 
 logger = logging.getLogger("django_alexa.views")
@@ -15,7 +14,7 @@ class CreateIntentSlots(fields.AmazonSlots):
     intent_type = fields.AmazonCustom(label="INTENT_TYPES", choices=INTENT_TYPES)
     to_city = fields.AmazonUSCity()
     from_city = fields.AmazonUSCity()
-    confirmation = fields.AmazonCustom(label="CONFIRMATION",choices=("yes","no"))
+    confirmation = fields.AmazonCustom(label="CONFIRMATION", choices=("yes", "no"))
 
 
 @intent(app="intents")
@@ -103,6 +102,7 @@ def CreateIntent(session, intent_type, from_city, to_city, confirmation):
                                                                                                  supported_locations[1])
             kwargs["end_session"] = False
             return ResponseBuilder.create_response(**kwargs)
+
     elif from_city is None or to_city is None:
         supported_locations = Customer.objects.values_list('location', flat=True)
         kwargs['message'] = "Between which two cities do you want the path?."
@@ -123,13 +123,16 @@ def CreateIntent(session, intent_type, from_city, to_city, confirmation):
     elif "yes" in confirmation:
         check = authenticate_intent()
         if check == "allow":
-            check_intent_status = create_intent(from_city,to_city,intent_type)
-            if check_intent_status == True:
-                kwargs['message'] = "The requested intent has been setup."
-            elif check_intent_status == False:
-                kwargs['message'] = "Some error occured in the bankend."
-        elif check == "deny":
-            kwargs['message'] = "The requested intent has been cancelled."
+            intent_type = generate_intent_type(intent_type)
+            check_duplicate_intent = check_exsiting_intent(from_city.upper(), to_city.upper())
+            if check_duplicate_intent:
+                kwargs['message'] = "An intent already exists between requested cities. Please restart for a different intent!"
+            else:
+                check_intent_status = create_intent(from_city.upper(), to_city.upper(), intent_type)
+                if check_intent_status == True:
+                    kwargs['message'] = "The requested intent has been setup."
+                elif check_intent_status == False:
+                    kwargs['message'] = "Some error occurred in the bank-end."
         kwargs.pop('from_city')
         kwargs.pop('intent_type')
         kwargs['launched'] = False
@@ -152,18 +155,35 @@ def CreateIntent(session, intent_type, from_city, to_city, confirmation):
     return ResponseBuilder.create_response(**kwargs)
 
 
-def create_intent(from_city,to_city,intent_type):
+def check_exsiting_intent(from_city, to_city):
+    try:
+
+        check_forward_path = Intent_Data.objects.filter(From_Location=from_city, To_Location=to_city)
+        check_reverse_path = Intent_Data.objects.filter(To_Location=from_city, From_Location=to_city)
+        if check_forward_path and check_reverse_path:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def generate_intent_type(intent_type):
     if "least latency" in intent_type.lower():
         intent_type = "least_latency"
     elif "high bandwidth" in intent_type.lower():
         intent_type = "high_bandwidth"
     elif "least hopcount" in intent_type.lower():
         intent_type = "least_hop_count"
+    return intent_type
+
+
+def create_intent(from_city, to_city, intent_type):
     try:
-        c = ComputeAndPush('10.0.1.200', from_city.upper(), to_city.upper(), intent_type)
+        c = ComputeAndPush('10.0.1.200', from_city, to_city, intent_type)
         status = c.intentEngine()
 
-        c2 = ComputeAndPush('10.0.1.200', to_city.upper(), from_city.upper(), intent_type)
+        c2 = ComputeAndPush('10.0.1.200', to_city, from_city, intent_type)
         status2 = c2.intentEngine()
 
         if status is not False and status2 is not False:
@@ -173,6 +193,7 @@ def create_intent(from_city,to_city,intent_type):
     except:
         return False
 
+
 def authenticate_intent():
     try:
         auth_client = duo_client.Auth(
@@ -180,7 +201,7 @@ def authenticate_intent():
             skey='QsufyeqbDYdI5Sh4EMkroCorfcANCMMoF3E5F10l',
             host="api-53df2292.duosecurity.com",
         )
-        status = auth_client.auth(device="auto",factor="push",username="amar")
+        status = auth_client.auth(device="auto", factor="push", username="amar")
         return status['status']
     except:
         return "deny"
